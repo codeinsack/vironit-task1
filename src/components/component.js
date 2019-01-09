6a1a82d386d17e7d187dbcd05b421a75332a8689
Dmitry Fiodorov <fedor.dmitry@gmail.com>
2019-01-09 09:54:42 +0300

    Comments

--- src/components/component.js
+++ src/components/component.js
@@ -1,9 +1,13 @@
 function Component() {}
 
 Component.prototype = {
+  // а для чего тогда params? может к html их нужно как-то применить?
+  // т.е. html лучше сделать как свойство класса, сделать функцию, которая по params будет формировать конечный html
+  // вцелом и params должно быть как свойство и меняться в в методе updateParams
   render: function(id, html, params) {
     var el = document.getElementById(id);
     if (!el) {
+      // а если нужно рендерить внутри какого-то div'a?
       document.body.innerHTML += html;
     } else {
       el.outerHTML = html;
